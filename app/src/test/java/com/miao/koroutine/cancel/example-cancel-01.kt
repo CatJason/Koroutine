03f9c86fec90aca3ee24730bf02c87ff6aad216f
CatJason <CatJason@github.com>
2023-07-03 10:52:17 +0800

    解析job.cancel()

--- app/src/test/java/com/miao/koroutine/cancel/example-cancel-01.kt
+++ app/src/test/java/com/miao/koroutine/cancel/example-cancel-01.kt
@@ -20,3 +20,14 @@ fun main() = runBlocking {
     job.join() // waits for job's completion 
     println("main: Now I can quit.")
 }
+
+/**
+ * 正确的理解是这样
+ * 调用 launch 函数执行传入的 CoroutineScope 的扩展函数，并返回 CoroutineScope 的子类 Job
+ * 协程开始执行
+ * 线程挂起，协程保持运行
+ * 线程唤醒
+ * 通过 job.cancel() 主动取消协程的运行
+ * 通过 job.join() 将线程加入到协程的执行当中，即协程执行完之后执行线程的内容
+ * 因为协程相关内容已经被主动取消，故只需执行后加入的线程的内容即可
+ */
