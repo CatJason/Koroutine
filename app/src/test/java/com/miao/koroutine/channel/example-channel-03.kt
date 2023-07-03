5017f99ee990272ed183b8be5af564440d2902ba
CatJason <CatJason@github.com>
2023-07-03 18:46:24 +0800

    使用 produce ：便捷的协程构建器，使用扩展函数 consumeEach 在消费者端替代 for 循环

--- app/src/test/java/com/miao/koroutine/channel/example-channel-03.kt
+++ app/src/test/java/com/miao/koroutine/channel/example-channel-03.kt
@@ -17,3 +17,9 @@ fun main() = runBlocking {
     squares.consumeEach { println(it) }
     println("Done!")
 }
+
+/**
+ * 将生产者抽象成一个函数，并且使通道作为它的参数
+ * 使用 produce ：便捷的协程构建器
+ * 使用扩展函数 consumeEach 在消费者端替代 for 循环
+ */
