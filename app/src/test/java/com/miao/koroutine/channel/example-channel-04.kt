2569912f5e7b396325c7600ad7ad14cbbaa0eef4
CatJason <CatJason@github.com>
2023-07-03 19:21:43 +0800

    管道 与 ReceiveChannel

--- app/src/test/java/com/miao/koroutine/channel/example-channel-04.kt
+++ app/src/test/java/com/miao/koroutine/channel/example-channel-04.kt
@@ -26,3 +26,18 @@ fun CoroutineScope.produceNumbers() = produce<Int> {
 fun CoroutineScope.square(numbers: ReceiveChannel<Int>): ReceiveChannel<Int> = produce {
     for (x in numbers) send(x * x)
 }
+
+/**
+ * 创建一个 produce numbers，无限的从 1 到 无穷 生产整数
+ * 创建一个 produce square，接收 numbers 作为 ReceiveChannel ，并进行处理
+ * squares.receive() 只曲其前 5
+ * 在 numbers，squares 挂起等待消费者时取消掉（注意协程的 cancel 机制，没有主动响应的只有在挂起时才能取消掉）
+ *
+ * numbers 是一个管道：一种一个协程在流中开始生产可能无穷多个元素的模式
+ */
+
+/**
+ * coroutineContext.cancelChildren()是一个协程上下文的扩展函数，用于取消该协程上下文中的所有子协程。
+ * 在这个示例中，main()函数创建了一个顶级协程作用域，其中包含了创建的生产者通道和接收者通道的协程。
+ * 当执行到coroutineContext.cancelChildren()时，它会取消所有子协程，即 numbers 和squares协程。
+ */
