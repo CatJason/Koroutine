f66f9f61f087aa3687dbda987c5aa7265f007771
CatJason <CatJason@github.com>
2023-07-05 04:36:30 +0800

    异常聚合

--- app/src/test/java/com/miao/koroutine/exceptions/example-exceptions-06.kt
+++ app/src/test/java/com/miao/koroutine/exceptions/example-exceptions-06.kt
@@ -30,3 +30,10 @@ fun main() = runBlocking {
     }
     job.join()
 }
+
+/**
+ * 在 Kotlin 协程中，默认情况下，取消异常是透明的并且未被包装。
+ * 当一个协程被取消时，它会抛出一个 CancellationException 异常。
+ * 这个异常并不会被包装在任何其他异常类型中，而是以原始的 CancellationException 形式抛出。
+ * 这种取消异常的透明性意味着你可以通过捕获 CancellationException 来判断协程是否被取消，并在需要的时候进行相应的处理。
+ */
