3e5740726e052142a5c085f175a46220edce2002
CatJason <CatJason@github.com>
2023-07-04 09:10:59 +0800

    使用 async 函数与 await() 来实现延迟计算和完成后恢复执行

--- app/src/test/java/com/miao/koroutine/compose/example-compose-02.kt
+++ app/src/test/java/com/miao/koroutine/compose/example-compose-02.kt
@@ -26,3 +26,11 @@ suspend fun doSomethingUsefulTwo(): Int {
     delay(1000L) // pretend we are doing something useful here, too
     return 29
 }
+
+/**
+ * 使用 async 函数与 await() 来实现延迟计算和完成后恢复执行
+ * async 函数返回一个 Deferred<T>
+ *     Deferred<T> 继承自 Job
+ *
+ * 其实 async 可以理解为 awaitSync 即等待同步执行
+ */
