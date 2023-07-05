36449f1be32976fb39b6fd4ecd1b422b8a684318
CatJason <CatJason@github.com>
2023-07-06 05:05:45 +0800

    flow {...} 构建器中的代码必须遵循上下文保存属性，并且不允许从其他上下文中发射（emit）

--- app/src/test/java/com/miao/koroutine/flow/example-flow-14.kt
+++ app/src/test/java/com/miao/koroutine/flow/example-flow-14.kt
@@ -21,3 +21,10 @@ fun simple(): Flow<Int> = flow {
 fun main() = runBlocking<Unit> {
     simple().collect { value -> println(value) } 
 }            
+
+/**
+ * 长时间运行的消耗 CPU 的代码也许需要在 Dispatchers.Default 上下文中执行，
+ * 并且更新 UI 的代码也许需要在 Dispatchers.Main 中执行
+ * 通常，withContext 用于在 Kotlin 协程中改变代码的上下文
+ * 但是 flow {...} 构建器中的代码必须遵循上下文保存属性，并且不允许从其他上下文中发射（emit）
+ */
