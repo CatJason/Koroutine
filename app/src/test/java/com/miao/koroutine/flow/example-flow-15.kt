4841a21ad1728ad28a549c2e5d67d0b384742969
CatJason <CatJason@github.com>
2023-07-06 05:07:40 +0800

    flowOn 函数，可用于更改流发射的上下文

--- app/src/test/java/com/miao/koroutine/flow/example-flow-15.kt
+++ app/src/test/java/com/miao/koroutine/flow/example-flow-15.kt
@@ -22,4 +22,9 @@ fun main() = runBlocking<Unit> {
     simple().collect { value ->
         log("Collected $value") 
     } 
-}            
+}
+
+/**
+ * 可以使用 flowOn 函数，该函数用于更改流发射的上下文。
+ * 以下示例展示了更改流上下文的正确方法，该示例还通过打印相应线程的名字以展示它们的工作方式。
+ */
