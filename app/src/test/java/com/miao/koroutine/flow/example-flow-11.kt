b4e80956a355694a85a27371a74503b6001bf570
CatJason <CatJason@github.com>
2023-07-06 04:09:17 +0800

    末端操作符

--- app/src/test/java/com/miao/koroutine/flow/example-flow-11.kt
+++ app/src/test/java/com/miao/koroutine/flow/example-flow-11.kt
@@ -14,3 +14,26 @@ fun main() = runBlocking<Unit> {
         .reduce { a, b -> a + b } // sum them (terminal operator)
     println(sum)
 }
+
+/**
+ * 末端操作符是在流上用于启动流收集的挂起函数。
+ * collect 是最基础的末端操作符，但是还有另外一些更方便使用的末端操作符：
+ */
+
+/**
+ * toList 与 toSet
+ * 转化为各种集合
+ *
+ * first
+ * first 操作符用于获取Flow中的第一个元素，并返回一个新的Flow，该Flow只包含第一个元素。
+ *
+ * single
+ * single 操作符用于确保Flow只发射单个元素，并返回该元素。
+ * 说人话就是用于Flow中只有一个数据的情况
+ *
+ * reduce
+ * 说人话就是可以用来累加，累乘，前提是按照一个统一的规则。
+ *
+ * fold
+ * 说人话就是多了一个起始数值，就相当于在最前面加了一个数
+ */
