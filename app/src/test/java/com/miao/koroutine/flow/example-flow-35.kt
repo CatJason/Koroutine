bed3ab43c44b15029a6cc0ff1869c2e60fbf0e58
CatJason <CatJason@github.com>
2023-07-06 22:42:44 +0800

    启动流

--- app/src/test/java/com/miao/koroutine/flow/example-flow-35.kt
+++ app/src/test/java/com/miao/koroutine/flow/example-flow-35.kt
@@ -16,4 +16,15 @@ fun main() = runBlocking<Unit> {
         .onEach { event -> println("Event: $event") }
         .collect() // <--- Collecting the flow waits
     println("Done")
-}            
+}
+
+/**
+ * 我们需要一个类似 addEventListener 的函数，该函数注册一段响应的代码处理即将到来的事件，并继续进行进一步的处理。
+ * onEach 操作符可以担任该角色。
+ *
+ * 然而，onEach 是一个过渡操作符。
+ * 我们也需要一个末端操作符来收集流。
+ * 否则仅调用 onEach 是无效的。
+ *
+ * 如果我们在 onEach 之后使用 collect 末端操作符，那么后面的代码会一直等待直至流被收集。
+ */
