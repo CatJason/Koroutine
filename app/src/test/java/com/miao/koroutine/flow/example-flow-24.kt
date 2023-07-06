0117e46e7001fabb6e7e12ef1dbd4c74151d85be
CatJason <CatJason@github.com>
2023-07-06 18:55:32 +0800

    flatMapMerge 和 flatMapConcat

--- app/src/test/java/com/miao/koroutine/flow/example-flow-24.kt
+++ app/src/test/java/com/miao/koroutine/flow/example-flow-24.kt
@@ -23,3 +23,23 @@ fun main() = runBlocking<Unit> {
             println("$value at ${currentTimeMillis() - startTime} ms from start") 
         } 
 }
+/**
+ * .flatMapMerge { requestFlow(it) } = map(requestFlow(it)).flattenMerge(concurrency)
+ */
+
+/**
+ * flatMapMerge 会顺序调用代码块（本示例中的 { requestFlow(it) }）
+ * 但是并发收集结果流，相当于执行顺序是首先执行 map { requestFlow(it) } 然后在其返回结果上调用 flattenMerge。
+ */
+
+/**
+ * flatMapMerge
+ * flatMapMerge操作符在处理源流时会并发地处理内部流，不保证内部流的顺序。
+ * 它会同时订阅和处理多个内部流，然后将它们的结果合并到单个输出流中。
+ * 这种并发处理可以提高处理速度，但内部流的顺序可能会被打乱。
+ *
+ * flatMapConcat
+ * flatMapConcat操作符在处理源流时会按照顺序逐个订阅和处理内部流，确保内部流的顺序保持不变。
+ * 它会等待前一个内部流完成后才会处理下一个内部流，确保按照顺序合并结果。
+ * 这种顺序处理可能会导致处理速度较慢，因为它必须等待每个内部流完成。
+ */
