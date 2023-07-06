10f331501fa0137eee9d5d36312a6e18b65ea06a
CatJason <CatJason@github.com>
2023-07-06 20:11:50 +0800

    异常的逃逸

--- app/src/test/java/com/miao/koroutine/flow/example-flow-29.kt
+++ app/src/test/java/com/miao/koroutine/flow/example-flow-29.kt
@@ -22,4 +22,9 @@ fun main() = runBlocking<Unit> {
             check(value <= 1) { "Collected $value" }                 
             println(value) 
         }
-}            
+}
+
+/**
+ * catch 过渡操作符遵循异常透明性，仅捕获上游异常（catch 操作符上游的异常，但是它下面的不是）。
+ * 如果 collect {...} 块（位于 catch 之下）抛出一个异常，那么异常会逃逸。
+ */
