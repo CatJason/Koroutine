dd3f5d71183c5585c63e76593f90ea1865d57291
CatJason <CatJason@github.com>
2023-07-06 23:05:06 +0800

    让繁忙的流可以取消

--- app/src/test/java/com/miao/koroutine/flow/example-flow-39.kt
+++ app/src/test/java/com/miao/koroutine/flow/example-flow-39.kt
@@ -14,3 +14,9 @@ fun main() = runBlocking<Unit> {
         println(value)
     } 
 }
+
+/**
+ * 在协程处于繁忙循环的情况下，必须明确检测是否取消。
+ * 可以添加 .onEach { currentCoroutineContext().ensureActive() }
+ * 但是这里提供了一个现成的 cancellable 操作符来执行此操作
+ */
