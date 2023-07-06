91fbac2d4a94757f81fe2393b657ebbea395fc63
CatJason <CatJason@github.com>
2023-07-06 10:07:20 +0800

    flatMapConcat 和 flattenConcat

--- app/src/test/java/com/miao/koroutine/flow/example-flow-23.kt
+++ app/src/test/java/com/miao/koroutine/flow/example-flow-23.kt
@@ -15,16 +15,38 @@ fun requestFlow(i: Int): Flow<String> = flow {
     emit("$i: Second")    
 }
 
-fun main() = runBlocking<Unit> { 
-    val startTime = currentTimeMillis() // remember the start time 
-    (1..3).asFlow().onEach { delay(100) } // emit a number every 100 ms 
-        .flatMapConcat { requestFlow(it) }                                                                           
-        .collect { value -> // collect and print 
-            println("$value at ${currentTimeMillis() - startTime} ms from start") 
-        } 
+/**
+ * flatMapConcat {...}
+    fun main() = runBlocking<Unit> {
+        val flowOfFlows: Flow<Flow<Int>> = flowOf(flowOf(1, 2), flowOf(3, 4))
+        val flatMapConcatResult = flowOfFlows.flatMapConcat { it }
+        flatMapConcatResult.collect { println(it) } // 输出：1, 2, 3, 4
+    }
+ * flattenConcat()
+    fun main() = runBlocking<Unit> {
+        val flowOfFlows: Flow<Flow<Int>> = flowOf(flowOf(1, 2), flowOf(3, 4))
+        val flattenConcatResult = flowOfFlows.flattenConcat()
+        flattenConcatResult.collect { println(it) } // 输出：1, 2, 3, 4
+    }
+ */
+
+fun main() = runBlocking<Unit> {
+    val startTime = currentTimeMillis() // remember the start time
+    (1..3).asFlow().onEach { delay(100) } // emit a number every 100 ms
+        .flatMapConcat { requestFlow(it) } // 等价于 map(requestFlow(it)).flattenConcat()
+        .collect { value -> // collect and print
+            println("$value at ${currentTimeMillis() - startTime} ms from start")
+        }
 }
 
 /**
- * 当一个流里包含了一个流，它就变得很难以理解，我们希望把这个嵌套的流结构给展平
- * Flow<Flow<String>>
+ * flatMapConcat操作符用于将原始Flow中的每个元素转换为另一个Flow，并将这些Flow连接和扁平化成一个新的Flow。
+ *
+ * requestFlow(it)：这是一个转换函数，它接收初始 Flow 中的每个元素，并返回一个新的 Flow。
+ * flatMapConcat 接收一个 Flow，返回 这个 Flow = map(requestFlow(it))
+ * .flatMapConcat { requestFlow(it) } = map(requestFlow(it)).flattenConcat()
  */
+
+
+
+
